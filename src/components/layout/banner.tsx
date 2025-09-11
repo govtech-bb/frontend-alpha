2b005366c8cd524eada28d1a5d223ca6b959bd12
Shannon Clarke <shannonajclarke@gmail.com>
2025-09-11 09:30:30 -0400

    chore: new banner design

--- src/components/layout/banner.tsx
+++ src/components/layout/banner.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import { Typography } from "../ui/typography";
+import { Typography } from "@/components/ui/typography";
 
 export const Banner = () => {
   return (
-    <div className="flex items-center justify-between bg-[#FFB700] px-4 py-[7px]">
+    <div className="flex items-center justify-between bg-[#00267F] px-4 py-[7px] text-white">
       <span className="flex items-center gap-2">
         <Image
           alt="flag"
@@ -11,10 +11,12 @@ export const Banner = () => {
           src="/images/coat-of-arms.png"
           width="17"
         />
-        <Typography variant="small">Official government website</Typography>
+        <Typography className="text-white" variant="small">
+          Official government website
+        </Typography>
       </span>
 
-      <Typography className="underline" variant="small">
+      <Typography className="text-white underline" variant="small">
         Learn More
       </Typography>
     </div>
