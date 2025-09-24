4ff0d5df2b5c5414628768b0c11d6ac2e104552e
Shannon Clarke <shannonajclarke@gmail.com>
2025-09-24 15:18:25 -0400

    chore: update homepage text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
         id="introduction"
       >
         <Typography variant="display">
-          How you find and use public services is changing
+          Government Alpha Services v1.0
         </Typography>
         <div className="space-y-4">
           <Typography variant="subheading">
