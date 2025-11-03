45f67838c36bea0b3d8648e50e0f96e964467ab8
Shannon Clarke <shannonajclarke@gmail.com>
2025-11-03 15:52:48 -0400

    fix: background color change

--- src/app/[...slug]/layout.tsx
+++ src/app/[...slug]/layout.tsx
@@ -7,11 +7,13 @@ export default function EntryPointLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="overflow-hidden bg-white lg:rounded-t-3xl">
-      <div className="container lg:space-y-16">
-        <BackButton className="py-6" />
-        {children}
-        <HelpfulBox />
+    <div className="bg-yellow-bright">
+      <div className="overflow-hidden bg-white lg:rounded-t-3xl">
+        <div className="container lg:space-y-16">
+          <BackButton className="py-6" />
+          {children}
+          <HelpfulBox />
+        </div>
       </div>
     </div>
   );
