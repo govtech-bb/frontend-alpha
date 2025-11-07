8b9e83501ecabbba6b640d7d928c8455d8597015
Aaron Harris <aaronah02@icloud.com>
2025-11-06 23:13:16 -0400

    refactor: update layout and styling across components for improved design consistency

--- src/app/[...slug]/layout.tsx
+++ src/app/[...slug]/layout.tsx
@@ -7,11 +7,12 @@ export default function EntryPointLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="bg-yellow-bright">
-      <div className="rounded-t-3xl bg-white">
-        <div className="container py-4 lg:space-y-16">
-          <BackButton className="py-6" />
+    <div className="bg-yellow-100">
+      <div className="rounded-t-3xl bg-neutral-white">
+        <div className="container space-y-8 overflow-hidden pt-4">
+          <BackButton />
           {children}
+
           <HelpfulBox />
         </div>
       </div>
