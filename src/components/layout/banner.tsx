6cd9a2326c18065049c5514cde100ea80fecd56e
Shannon Clarke <shannonajclarke@gmail.com>
2025-11-11 04:12:11 -0400

    fix: width of header banner

--- src/components/layout/banner.tsx
+++ src/components/layout/banner.tsx
@@ -10,23 +10,25 @@ export const Banner = () => {
   const isOnHomePage = pathname === "/";
   return (
     <div
-      className={`container bg-blue-100 ${isOnHomePage ? "pb-2" : ""} text-neutral-white`}
+      className={`bg-blue-100 ${isOnHomePage ? "pb-2" : ""} text-neutral-white`}
     >
-      <div className="flex items-center justify-between py-2">
-        <span className="flex items-center gap-2">
-          <Image
-            alt="flag"
-            className="block"
-            height="16"
-            src="/images/coat-of-arms.png"
-            width="17"
-          />
-          <Typography className="text-neutral-white" variant="small">
-            Official government website
-          </Typography>
-        </span>
+      <div className="container">
+        <div className="flex items-center justify-between py-2">
+          <span className="flex items-center gap-2">
+            <Image
+              alt="flag"
+              className="block"
+              height="16"
+              src="/images/coat-of-arms.png"
+              width="17"
+            />
+            <Typography className="text-neutral-white" variant="small">
+              Official government website
+            </Typography>
+          </span>
+        </div>
+        {isOnHomePage ? <StageBanner className="py-2" stage="alpha" /> : null}
       </div>
-      {isOnHomePage ? <StageBanner className="py-2" stage="alpha" /> : null}
     </div>
   );
 };
