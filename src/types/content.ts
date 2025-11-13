9b04cec45156172cfb8c679edacd9f6d5fad17ec
Shannon Clarke <shannonajclarke@gmail.com>
2025-11-13 12:06:23 -0400

    feat: dynamically display markdown files and components via wildcard slugs

--- src/types/content.ts
+++ src/types/content.ts
@@ -5,6 +5,7 @@ export type PageType = {
   slug: string;
   source_url?: string;
   description: string; // date when content was published, updated or migrated
+  subPages?: { slug: string; type: "markdown" | "component" }[];
 };
 
 export type ServiceCategoryType = {
