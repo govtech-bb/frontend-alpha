7c242796acb857b2376115d9cb14f94c3de32121
Shannon Clarke <shannonajclarke@gmail.com>
2025-12-09 09:41:12 -0400

    chore: update form schema definition

--- src/schema/jobstart-plus-programme.ts
+++ src/schema/jobstart-plus-programme.ts
@@ -3,40 +3,20 @@ import type { FormStep } from "@/types";
 
 export const formSteps: FormStep[] = [
   {
-    id: "eligibility",
-    title: "Eligibility",
-    fields: [
-      {
-        name: "eligibility.age",
-        label: "Age",
-        type: "number",
-        placeholder: "Enter your age",
-        validation: {
-          required: "Number of years is required",
-        },
-      },
-    ],
-  },
-  {
     id: "applicant-details",
-    title: "Applicant Details",
+    title: "Tell us about yourself",
     fields: [
       {
-        name: "name.firstName",
+        name: "applicant.firstName",
         label: "First Name",
         type: "text",
         validation: {
           required: "First name is required",
         },
       },
+
       {
-        name: "name.middleName",
-        label: "Middle Name",
-        type: "text",
-        validation: {},
-      },
-      {
-        name: "name.lastName",
+        name: "applicant.lastName",
         label: "Last Name",
         type: "text",
         validation: {
@@ -44,7 +24,58 @@ export const formSteps: FormStep[] = [
         },
       },
       {
-        name: "name.gender",
+        name: "applicant.dateOfBirth",
+        label: "Date of Birth",
+        type: "date",
+        validation: {
+          required: "Date of birth is required",
+        },
+      },
+
+      {
+        name: "applicant.idNumber",
+        label: "ID Number",
+        type: "text",
+        placeholder: "",
+        validation: {
+          required: "ID Number is required",
+          minLength: {
+            value: 2,
+            message: "ID Number must be at least 2 characters",
+          },
+        },
+      },
+      {
+        name: "applicant.hasNisNumber",
+        label: "Do you have a National Insurance Number (NIS)?",
+        type: "radio",
+        validation: {
+          required: "NIS Number is required",
+        },
+        options: [
+          { label: "Yes", value: "yes" },
+          { label: "No", value: "no" },
+        ],
+      },
+      {
+        name: "applicant.nisNumber",
+        label: "What is your National Insurance Number (NIS)?",
+        type: "text",
+        placeholder: "",
+        validation: {
+          required: "NIS Number is required",
+          minLength: {
+            value: 2,
+            message: "NIS Number must be at least 2 characters",
+          },
+        },
+        conditionalOn: {
+          field: "applicant.hasNisNumber",
+          value: "yes",
+        },
+      },
+      {
+        name: "applicant.gender",
         label: "Sex",
         type: "radio",
         validation: {
@@ -56,73 +87,61 @@ export const formSteps: FormStep[] = [
         ],
       },
       {
-        name: "personal.dateOfBirth",
-        label: "Date of Birth",
-        type: "date",
-        validation: {
-          required: "Date of birth is required",
-        },
-      },
-      {
-        name: "personal.benNumber",
-        label: "BEN Number",
-        type: "text",
-        validation: {},
-      },
-      {
-        name: "personal.nisNumber",
-        label: "NIS Number",
-        type: "text",
-        validation: {},
-      },
-      {
-        name: "personal.maritalStatus",
+        name: "applicant.maritalStatus",
         label: "Marital Status",
-        type: "text",
-        validation: {},
-      },
-      {
-        name: "contact.landline",
-        label: "Landline",
-        type: "tel",
-        validation: {},
-      },
-      {
-        name: "contact.cellular",
-        label: "Cellular",
-        type: "tel",
+        type: "select",
         validation: {
-          required: "Cellular number is required",
+          required: "Relationship is required",
         },
+        options: [
+          { label: "Select marital status", value: "" },
+          { label: "Single", value: "single" },
+          { label: "Married", value: "married" },
+          { label: "Divorced", value: "divorced" },
+        ],
       },
       {
-        name: "contact.email",
-        label: "Email",
-        type: "text",
+        name: "applicant.hasDisability",
+        label: "Do you have a disability?",
+        type: "radio",
         validation: {
-          required: "Email address is required",
-          pattern: {
-            value: ".+@.+\\..+",
-            message: "Enter a valid email address",
+          required: "Disability status is required",
+        },
+        options: [
+          { label: "Yes", value: "yes" },
+          { label: "No", value: "no" },
+        ],
+      },
+    ],
+  },
+  {
+    id: "contact-details",
+    title: "Your Contact Details",
+    description: "Description",
+    fields: [
+      {
+        name: "contact.addressLine1",
+        label: "Address Line 1",
+        type: "text",
+        placeholder: "",
+        validation: {
+          required: "Address line 1 is required",
+          minLength: {
+            value: 5,
+            message: "Address must be at least 5 characters",
           },
         },
       },
       {
-        name: "address.addressLine1",
-        label: "Address Line 1",
-        type: "text",
-        validation: {
-          required: "Address line 1 is required",
-        },
-      },
-      {
-        name: "address.addressLine2",
+        name: "contact.addressLine2",
         label: "Address Line 2",
         type: "text",
+        placeholder: "",
         validation: {},
       },
+
       {
-        name: "address.parish",
+        name: "contact.parish",
         label: "Parish",
         type: "select",
         validation: {
@@ -130,83 +149,109 @@ export const formSteps: FormStep[] = [
         },
         options: barbadosParishes,
       },
+      {
+        name: "contact.postalCode",
+        label: "Postal Code",
+        type: "number",
+        placeholder: "",
+        validation: {},
+      },
+      {
+        name: "contact.isMailingAddressSame",
+        label: "Is your mailing address the same as above?",
+        type: "radio",
+        validation: {
+          required: "Field is required",
+        },
+        options: [
+          { label: "Yes", value: "yes" },
+          { label: "No", value: "no" },
+        ],
+      },
     ],
   },
   {
-    id: "criteria-and-entitlement",
-    title: "Criteria and Entitlement",
+    id: "emergency-contact",
+    title: "Emergency Contact Details",
+    description: "Description",
     fields: [
       {
-        name: "eligibility.areasOfInterest",
-        label: "Areas of Interest",
+        name: "emergency.firstName",
+        label: "First Name",
         type: "text",
-        placeholder: "Enter your areas of interest",
-        validation: {},
+        validation: {
+          required: "First name is required",
+        },
       },
       {
-        name: "eligibility.willingToWorkAtNight",
-        label: "Willing to Work at Night",
+        name: "emergency.lastName",
+        label: "Last Name",
         type: "text",
-        placeholder: "Yes or No",
-        validation: {},
+        validation: {
+          required: "Last name is required",
+        },
       },
       {
-        name: "education.primarySchool",
-        label: "Primary School (Name/Years)",
-        type: "text",
-        validation: {},
+        name: "emergency.relationship",
+        label: "Relationship ",
+        type: "select",
+        validation: {
+          required: "Relationship is required",
+        },
+        options: [
+          { label: "Select relationship", value: "" },
+          { label: "Mother", value: "mother" },
+          { label: "Father", value: "father" },
+          { label: "Grandmother", value: "grandmother" },
+          { label: "Grandfather", value: "grandfather" },
+          { label: "Aunt", value: "aunt" },
+          { label: "Uncle", value: "uncle" },
+          { label: "Legal Guardian", value: "legal_guardian" },
+          { label: "Other", value: "other" },
+        ],
       },
       {
-        name: "education.secondarySchool",
-        label: "Secondary School (Name/Years)",
+        name: "emergency.addressLine1",
+        label: "Address Line 1",
         type: "text",
-        validation: {},
+        placeholder: "",
+        validation: {
+          required: "Address line 1 is required",
+          minLength: {
+            value: 5,
+            message: "Address must be at least 5 characters",
+          },
+        },
       },
       {
-        name: "education.subjectsStudied",
-        label: "Subjects Studied",
+        name: "emergency.addressLine2",
+        label: "Address Line 2",
         type: "text",
+        placeholder: "",
         validation: {},
       },
+
       {
-        name: "employment.employerName",
-        label: "Employer Name",
-        type: "text",
-        validation: {},
+        name: "emergency.parish",
+        label: "Parish",
+        type: "select",
+        validation: {
+          required: "Parish is required",
+        },
+        options: barbadosParishes,
       },
       {
-        name: "employment.position",
-        label: "Position",
-        type: "text",
-        validation: {},
-      },
-      {
-        name: "employment.responsibilities",
-        label: "Responsibilities",
-        type: "text",
-        validation: {},
-      },
-      {
-        name: "employment.duration",
-        label: "Duration",
-        type: "text",
-        validation: {},
-      },
-      {
-        name: "disability.isDisabled",
-        label: "Are You Disabled?",
-        type: "text",
-        placeholder: "Yes or No",
-        validation: {},
-      },
-      {
-        name: "disability.type",
-        label: "Disability Type",
-        type: "text",
-        validation: {},
-        conditionalOn: {
-          field: "disability.isDisabled",
-          value: "Yes",
+        name: "emergency.telephoneNumber",
+        label: "Phone Number",
+        type: "tel",
+        placeholder: "+1 (246) 234-5678",
+        validation: {
+          required: "Telephone number is required",
+          pattern: {
+            value: "^\\+1\\s?\\(?246\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$",
+            message:
+              "Please enter a valid Barbados phone number (e.g., +1 246 234 5678)",
+          },
         },
       },
     ],
