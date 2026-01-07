dc9428d7a2b2ca17ce9372122868916d35848afb
Shannon Clarke <shannonajclarke@gmail.com>
2026-01-07 13:13:53 -0400

    chore: add e2e tests

---
+++ tests/e2e/sell-goods-services-beach-park-generic.spec.ts
@@ -0,0 +1,14 @@
+import { createFormTest, type FormTestConfig } from "./generic-form.spec";
+import { formSteps } from "@/schema/sell-goods-services-beach-park";
+
+/**
+ * Sell Goods or Services at Beach or Park Form Test using Generic Test Framework
+ */
+
+const config: FormTestConfig = {
+  schemaPath: "@/schema/sell-goods-services-beach-park",
+  formUrl: "/work-employment/sell-goods-services-beach-park/form",
+  formName: "Sell Goods or Services at Beach or Park",
+};
+
+createFormTest(config, formSteps);
