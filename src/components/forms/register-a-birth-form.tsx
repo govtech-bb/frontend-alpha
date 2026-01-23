eae7e73567daa22f68b1b84ef987b71f731db6cc
Aaron Harris <aaronah02@icloud.com>
2026-01-23 08:52:27 -0400

    feat: implement birth registration form and associated schema

---
+++ src/components/forms/register-a-birth-form.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import DynamicMultiStepForm from "@/components/forms/builder/multi-step-form";
+import { formSteps } from "@/schema/register-a-birth";
+
+export default function RegisterABirthForm() {
+  return (
+    <DynamicMultiStepForm
+      formSteps={formSteps}
+      serviceTitle="Register a birth"
+      storageKey="register-a-birth"
+    />
+  );
+}
