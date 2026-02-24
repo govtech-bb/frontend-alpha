11a6787c0cabb124f8602602b9db2dc36e3b4351
ihtishamtanveer <ihtishamtanveer992@gmail.com>
2026-02-24 15:08:29 +0500

    fix: remove unused 'op' dependency from payment error tracking

--- src/components/payment-block.tsx
+++ src/components/payment-block.tsx
@@ -52,7 +52,7 @@ export const PaymentBlock = ({ paymentData, formId }: Props) => {
       TRACKED_EVENTS.PAYMENT_ERROR_EVENT,
       getFormBaseContext(formId, categorySlug)
     );
-  }, [op, formId, categorySlug]);
+  }, [formId, categorySlug]);
 
   // Check transaction status on mount if URL params are present
   useEffect(() => {
