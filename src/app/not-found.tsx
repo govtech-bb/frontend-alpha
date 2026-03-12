58576bbd3d32cb00fdec23314ef64c5e6b25faf1
Aaron Harris <aaronah02@icloud.com>
2026-03-12 13:44:15 -0400

    style: update suggestions list and restore link button for service directory

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -17,20 +17,20 @@ export default function NotFound() {
         <div className="space-y-s">
           <Heading as="h3">Suggestions:</Heading>
 
-          <ul className="space-y-s">
+          <ul className="list-disc space-y-s pl-7.5">
             <li>Check the web address for typos</li>
             <li>Return to the homepage</li>
-            {/* <li>Browse our services directory</li> */}
+            <li>Browse our services directory</li>
           </ul>
         </div>
 
         {/* Quick Link Buttons */}
         <div className="flex flex-col gap-xm md:flex-row">
-          {/* <LinkButton href="/" variant="secondary">
+          <LinkButton href="/search" variant="secondary">
             Browse service directory
-          </LinkButton> */}
+          </LinkButton>
           <LinkButton href="/" variant="primary">
-            Return to homepage
+            Return to the homepage
           </LinkButton>
         </div>
       </div>
